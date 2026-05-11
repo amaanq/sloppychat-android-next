amaanq/schildichat-android-next

feat: mark all rooms as read

features/home/impl/src/main/kotlin/io/element/android/features/home/impl/roomlist/RoomListPresenter.kt
@@ -83,6 +83,9 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.takeWhile
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Semaphore
+import kotlinx.coroutines.sync.withPermit
+import timber.log.Timber
 
 private const val EXTENDED_RANGE_SIZE = 40
 private const val SUBSCRIBE_TO_VISIBLE_ROOMS_DEBOUNCE_IN_MILLIS = 300L
@@ -166,6 +169,7 @@ class RoomListPresenter(
                 }
                 is RoomListEvents.SetRoomIsFavorite -> coroutineScope.setRoomIsFavorite(event.roomId, event.isFavorite)
                 is RoomListEvents.SetRoomIsLowPriority -> coroutineScope.launch { client.getRoom(event.roomId)?.use { it.setIsLowPriority(event.isLowPriority) } } // SC
+                is RoomListEvents.MarkAllAsRead -> coroutineScope.markAllAsRead() // SC
                 is RoomListEvents.MarkAsRead -> coroutineScope.markAsRead(event.roomId)
                 is RoomListEvents.MarkAsUnread -> coroutineScope.markAsUnread(event.roomId)
                 is RoomListEvents.AcceptInvite -> {
@@ -353,6 +357,32 @@ class RoomListPresenter(
                 }
         }
     }
+
+    // SC start
+    private fun CoroutineScope.markAllAsRead() = launch {
+        val receiptType = if (sessionPreferencesStore.isSendPublicReadReceiptsEnabled().first()) {
+            ReceiptType.READ
+        } else {
+            ReceiptType.READ_PRIVATE
+        }
+        val semaphore = Semaphore(10)
+        client.getJoinedRoomIds().getOrNull()?.forEach { roomId ->
+            launch {
+                semaphore.withPermit {
+                    runCatching {
+                        notificationCleaner.clearMessagesForRoom(client.sessionId, roomId)
+                        client.getRoom(roomId)?.use { room ->
+                            room.setUnreadFlag(isUnread = false)
+                            room.markAsRead(receiptType)
+                        }
+                    }.onFailure { e ->
+                        Timber.w(e, "Failed to mark room $roomId as read")
+                    }
+                }
+            }
+        }
+    }
+    // SC end
 
     private fun CoroutineScope.markAsUnread(roomId: RoomId) = launch {
         client.getRoom(roomId)?.use { room ->

amaanq/sloppychat-android-next

features/home/impl/src/main/kotlin/io/element/android/features/home/impl/roomlist/RoomListPresenter.kt
@@ -82,6 +82,9 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.takeWhile
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Semaphore
+import kotlinx.coroutines.sync.withPermit
+import timber.log.Timber
 
 @Inject
 class RoomListPresenter(
@@ -166,6 +169,7 @@ class RoomListPresenter(
                 }
                 is RoomListEvent.SetRoomIsFavorite -> coroutineScope.setRoomIsFavorite(event.roomId, event.isFavorite)
                 is RoomListEvent.SetRoomIsLowPriority -> coroutineScope.launch { client.getRoom(event.roomId)?.use { it.setIsLowPriority(event.isLowPriority) } } // SC
+                is RoomListEvent.MarkAllAsRead -> coroutineScope.markAllAsRead() // SC
                 is RoomListEvent.MarkAsRead -> coroutineScope.markAsRead(event.roomId)
                 is RoomListEvent.MarkAsUnread -> coroutineScope.markAsUnread(event.roomId)
                 is RoomListEvent.AcceptInvite -> {
@@ -360,6 +364,39 @@ class RoomListPresenter(
                 }
         }
     }
+
+    // SC start
+    private fun CoroutineScope.markAllAsRead() = launch {
+        val receiptType = if (sessionPreferencesStore.isSendPublicReadReceiptsEnabled().first()) {
+            ReceiptType.READ
+        } else {
+            ReceiptType.READ_PRIVATE
+        }
+        val semaphore = Semaphore(10)
+        client.getJoinedRoomIds().getOrNull()?.forEach { roomId ->
+            launch {
+                semaphore.withPermit {
+                    runCatching {
+                        client.getRoom(roomId)?.use { room ->
+                            // Try the network/SDK side first. Only clear local state
+                            // (notifications + unread flag) once the receipt advance
+                            // actually succeeded — otherwise the UI lies on failure.
+                            val result = room.markAsRead(receiptType)
+                            if (result.isSuccess) {
+                                room.setUnreadFlag(isUnread = false)
+                                notificationCleaner.clearMessagesForRoom(client.sessionId, roomId)
+                            } else {
+                                Timber.w(result.exceptionOrNull(), "markAsRead failed for $roomId; leaving unread state intact")
+                            }
+                        }
+                    }.onFailure { e ->
+                        Timber.w(e, "Failed to mark room $roomId as read")
+                    }
+                }
+            }
+        }
+    }
+    // SC end
 
     private fun CoroutineScope.markAsUnread(roomId: RoomId) = launch {
         client.getRoom(roomId)?.use { room ->